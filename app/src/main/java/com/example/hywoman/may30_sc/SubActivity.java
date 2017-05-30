eee35a680d6343d4c971db66cc15ed6aa76322a8
tjsrkdmf <tjsrkdmf0208@gmail.com>
2017-05-30 18:25:01 +0900

    아무거나

---
+++ app/src/main/java/com/example/hywoman/may30_sc/SubActivity.java
@@ -0,0 +1,13 @@
+package com.example.hywoman.may30_sc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+    }
+}
